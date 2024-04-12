825d28f7315fe382d5bece3881f7a02313fb4f42
Shobhit Singh <majin.shobhitrawat53@gmail.com>
2024-04-13 00:40:11 +0530

    database connection is done

---
+++ database/databaseConnection.go
@@ -0,0 +1,52 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// DBinstance func
+func DBinstance() *mongo.Client {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	MongoDb := os.Getenv("MONGODB_URL")
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, canecl := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer canecl()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connected to mongodb !")
+	return client
+}
+
+// client database instance
+var Client *mongo.Client = DBinstance()
+
+// OpenCollection is a  function makes a connection with a collection in the database
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("cluster0").Collection(collectionName)
+
+	return collection
+
+}
