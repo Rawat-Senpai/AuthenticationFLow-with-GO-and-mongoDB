20c9c15b915a87334248650a2e22242ce9e57eac
Shobhit Singh <majin.shobhitrawat53@gmail.com>
2024-04-15 13:32:34 +0530

    User model updated
    added bson so that I am able to change things without getting dublicacy

--- models/userModel.go
+++ models/userModel.go
@@ -7,14 +7,14 @@ import (
 )
 
 type User struct {
-	ID            primitive.ObjectID `bson:"_id"`
-	Name          *string            `json:"name" validate:"required,min=2,max=20"`
-	Passowrd      *string            `json:"password" validate:"required"`
-	Email         *string            `json:"email" validate:"required"`
-	Token         *string            `json:"token"`
-	Refresh_token *string            `json:"refresh_token"`
-	User_id       string             `json:"user_id"`
-	Created_at    time.Time          `json:"created_at"`
-	Updated_at    time.Time          `json:"updated_at"`
-	Phone         *string            `json:"phone"`
+	ID            primitive.ObjectID `bson:"_id" json:"UserId"`
+	Name          *string            `bson:"name" json:"name" validate:"required,min=2,max=20"`
+	Passowrd      *string            `bson:"password" json:"password" validate:"required"`
+	Email         *string            `bson:"email" json:"email" validate:"required"`
+	Token         *string            `bson:"token" json:"token"`
+	Refresh_token *string            `bson:"refresh_token" json:"refresh_token"`
+	User_id       string             `bson:"user_id" json:"user_id"`
+	Created_at    time.Time          `bson:"created_at" json:"created_at"`
+	Updated_at    time.Time          `bson:"updated_at" json:"updated_at"`
+	Phone         *string            `bson:"phone" json:"phone"`
 }
